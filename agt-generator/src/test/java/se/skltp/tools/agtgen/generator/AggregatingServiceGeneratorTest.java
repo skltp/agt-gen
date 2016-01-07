91b00cc97fe0d95d3f80f581dcc38f625458bd16
Martin Flower <martin.flower@callistaenterprise.se>
2016-01-07 11:36:09 +0100

    Updated to work with git instead of subversion

--- agt-generator/src/test/java/se/skltp/tools/agtgen/generator/AggregatingServiceGeneratorTest.java
+++ agt-generator/src/test/java/se/skltp/tools/agtgen/generator/AggregatingServiceGeneratorTest.java
@@ -20,14 +20,13 @@
  */
 package se.skltp.tools.agtgen.generator;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import static org.soitoolkit.tools.generator.model.enums.DeploymentModelEnum.STANDALONE_DEPLOY;
 import static org.soitoolkit.tools.generator.util.SystemUtil.BUILD_COMMAND;
 import static org.soitoolkit.tools.generator.util.SystemUtil.CLEAN_COMMAND;
 import static org.soitoolkit.tools.generator.util.SystemUtil.ECLIPSE_AND_TEST_REPORT_COMMAND;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.After;
@@ -42,97 +41,71 @@ import org.soitoolkit.tools.generator.util.SystemUtil;
 
 public class AggregatingServiceGeneratorTest {
 
-	private static final String TEST_OUT_FOLDER = PreferencesUtil.getDefaultRootFolder() + "/jUnitTests";
-	private static final String VERSION = "1.0.0-SNAPSHOT";
+    private static final String TEST_OUT_FOLDER = PreferencesUtil.getDefaultRootFolder() + "/jUnitTests";
+    private static final String VERSION = "1.0.0-SNAPSHOT";
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-    /**
-	 * 
-	 * @throws java.io.IOException
-	 */
-	@Test
-	public void testAggregatingServices1() throws IOException {
+    @Test
+    public void testGenerateGetAggregatedRequestActivities() throws IOException {
+        doTestAggregatingServices("riv.crm.requeststatus", "GetAggregatedRequestActivities", MuleVersionEnum.MULE_3_5_0, STANDALONE_DEPLOY);
+    }
 
-		doTestAggregatingServices("riv.crm.requeststatus", "GetAggregatedRequestActivities", MuleVersionEnum.MULE_3_5_0, STANDALONE_DEPLOY);
+    @Test
+    public void testGenerateGetAggregatedActivities() throws IOException {
+        doTestAggregatingServices("riv.clinicalprocess.activity.actions", "GetAggregatedActivities", MuleVersionEnum.MULE_3_5_0, STANDALONE_DEPLOY);
+    }
 
-	}
+    private void doTestAggregatingServices(String domainId, String artifactId, MuleVersionEnum muleVersion, DeploymentModelEnum deploymentModel) throws IOException {
+        String projectFolder = TEST_OUT_FOLDER + "/" + artifactId + "-TEST";
+        createAggregationService(domainId, artifactId, muleVersion, projectFolder);
+        performMavenBuild(projectFolder + "/" + domainId + "." + artifactId);
+    }
 
-    /**
-	 * 
-	 * @throws java.io.IOException
-	 */
-	@Test
-	public void testAggregatingServices2() throws IOException {
-
-		doTestAggregatingServices("riv.clinicalprocess.activity.actions", "GetAggregatedActivity", MuleVersionEnum.MULE_3_5_0, STANDALONE_DEPLOY);
-
-		/*
-		MuleVersionEnum[] muleVersions = MuleVersionEnum.values();
-		for (int i = 0; i < muleVersions.length; i++) {
-			if (!muleVersions[i].isEEVersion()) {
-				doTestAggregationServices("se.skltp.aggregatingservices.clinicalprocess.activity.actions", "GetAggregatedGetActivity" + muleVersions[i].getVerNoNumbersOnly(), muleVersions[i], STANDALONE_DEPLOY);
-			}
-		}
-		*/
-	}
-
-	private void doTestAggregatingServices(String domainId, String artifactId, MuleVersionEnum muleVersion, DeploymentModelEnum deploymentModel) throws IOException {
-
-		String projectFolder = TEST_OUT_FOLDER + "/" + artifactId + "-TEST";
-		createAggregationService(domainId, artifactId, muleVersion, projectFolder);
-		//GetAggregatedGetActivity-TEST/riv.clinicalprocess.activity.actions/GetAggregatedGetActivity/trunk
-		performMavenBuild(projectFolder + "/" + domainId + "/" + artifactId + "/trunk");
-	}
-
-	private void createAggregationService(String domainId, String artifactId, MuleVersionEnum muleVersion, String projectFolder) throws IOException {
-
-		SystemUtil.delDirs(projectFolder);
-
+    private void createAggregationService(String domainId, String artifactId, MuleVersionEnum muleVersion, String projectFolder) throws IOException {
+        SystemUtil.delDirs(projectFolder);
         boolean genSchema = true;
-
-		int expectedNoOfFiles1 = 91; // For domain + subdomain
-		int expectedNoOfFiles2 = 94; // For domain + subdomain + subdomain
-		
-		int noOfFilesBefore = SystemUtil.countFiles(projectFolder);
-
+        int expectedNoOfFiles1 = 89; // For domain + subdomain
+        int expectedNoOfFiles2 = 92; // For domain + subdomain + subdomain
+        int noOfFilesBefore = SystemUtil.countFiles(projectFolder);
+        
         new AggregatingServiceGenerator(System.out, domainId, artifactId, VERSION, muleVersion, projectFolder, genSchema, null, null).startGenerator();
-				
-		int actualNoOfFiles = SystemUtil.countFiles(projectFolder) - noOfFilesBefore;
-		
-		assertTrue("Missmatch in expected number of created files and folders."
-				+ " Expected " + expectedNoOfFiles1 + " or " + expectedNoOfFiles2 + " but found " + actualNoOfFiles,
-				actualNoOfFiles == expectedNoOfFiles1 || actualNoOfFiles == expectedNoOfFiles2);
-	}
 
-	private void performMavenBuild(String projectFolder) throws IOException {
+        int actualNoOfFiles = SystemUtil.countFiles(projectFolder) - noOfFilesBefore;
+        assertTrue("Missmatch in expected number of created files and folders." + " Expected " + expectedNoOfFiles1 + " or " + expectedNoOfFiles2
+                + " but found " + actualNoOfFiles, actualNoOfFiles == expectedNoOfFiles1 || actualNoOfFiles == expectedNoOfFiles2);
+    }
 
-		boolean testOk = false;
-		
-		try {
-			SystemUtil.executeCommand(BUILD_COMMAND, projectFolder);
-			testOk = true;
-		} finally {
-			// Always try to create eclipsefiles and test reports 
-			SystemUtil.executeCommand(ECLIPSE_AND_TEST_REPORT_COMMAND, projectFolder);
-		}
-		
-		// If the build runs fine then also perform a clean-command to save GB's of diskspace...
-		if (testOk) SystemUtil.executeCommand(CLEAN_COMMAND, projectFolder);
-	}
+    private void performMavenBuild(String projectFolder) throws IOException {
+        boolean testOk = false;
+        
+        try {
+            SystemUtil.executeCommand(BUILD_COMMAND, projectFolder);
+            testOk = true;
+        } finally {
+            // Always try to create eclipsefiles and test reports
+            String command = ECLIPSE_AND_TEST_REPORT_COMMAND.replaceAll("studio:studio", "eclipse:eclipse");
+            SystemUtil.executeCommand(command, projectFolder);
+        }
+
+        // If the build runs fine then also perform a clean-command to save GB's of diskspace...
+        if (testOk) {
+            SystemUtil.executeCommand(CLEAN_COMMAND, projectFolder);
+        }
+    }
 
 }
